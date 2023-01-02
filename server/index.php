f47b3d01c829682d39f275622984e19d926c08e5
Marwan Ehab <marwan.el.sheikh14@gmail.com>
2023-01-02 19:11:29 +0200

    Update index.php
    remove unused code

--- server/index.php
+++ server/index.php
@@ -3,7 +3,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Credentials: true');
-// header('Access-Control-Max-Age: 86400');
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
 
@@ -26,7 +25,6 @@ switch ($method) {
             $product = json_decode(file_get_contents('php://input'));
             $type = $product->type;
             $class = $type;
-            // echo json_encode($path);
             $product_object = new $class($product, true);
             $product_object->addProduct($conn, $type);
         } else {
@@ -34,8 +32,4 @@ switch ($method) {
             $products_list->deleteProduct($conn, $path[2]);
         }
         break;
-    // case "DELETE":
-    //     $products_list = new ProductList();
-    //     $products_list->deleteProduct($conn);
-    //     break;
 }
